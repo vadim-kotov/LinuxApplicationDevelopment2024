8d24889663dc7524a78e8b559626147d820e53c1
Vadim Kotov <vadia55@list.ru>
2025-01-13 21:04:49 +0300

    getlines_from_file function

--- 01_TerminalProject/Show.c
+++ 01_TerminalProject/Show.c
@@ -3,9 +3,45 @@
 #define DX 2 
 #define DY 2
 
+**char getlines_from_file(char *filename)
+{
+	FILE *f;
+	char *line = NULL;
+	size_t len = 0;
+	int nlines = 128;
+	int i;
+	char **lines = malloc(sizeof(char *) * nlines);
+
+	f = fopen(filename, "r");
+	if(f == NULL)
+	{
+		perror("fopen() ");
+		return NULL;
+	}
+
+	i = 0;
+	while((getline(&line, &len, f)) != -1)
+	{
+		lines[i] = line;
+		i++;
+		if(i >= nlines - 1)
+		{
+			nlines = lines * 2;
+			new_lines = realloc(lines, sizeof(char *) * nlines);
+			if(new_lines == NULL)
+			{
+				//TODO free
+				perror("realloc() ");
+				return NULL;
+			}
+			lines = new_lines;
+		}
+	}
+}
+
 int main(int argc, char **argv) 
 {
-	WINDOW *win;
+	WINDOW *frame, *win;
 
 	initscr();
 	noecho();
@@ -14,9 +50,12 @@ int main(int argc, char **argv)
 	printw("File: %s", argv[1]);
 	refresh();
 
-	win = newwin(LINES - 2 * DY, COLS - 2 * DX, DY, DX);
-	box(win, 0, 0);
-	wmove(win, 0, 1);
+	frame = newwin(LINES - 2 * DY, COLS - 2 * DX, DY, DX);
+	box(frame, 0, 0);
+	wrefresh(frame);
+
+	win = newwin(LINES - 2 * DY - 2, COLS - 2 * DX - 2, DY + 1, DX + 1);
+	//wmove(win, 0, 1);
 	//wrefresh(win);
 	wgetch(win);
 	endwin();
