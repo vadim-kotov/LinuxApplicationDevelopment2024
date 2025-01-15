dc4de9a22e722c23e0980e96ca194b117ba9bc33
Vadim Kotov <vadia55@list.ru>
2025-01-15 23:41:35 +0300

    getlines_from_file finished

--- 01_TerminalProject/Show.c
+++ 01_TerminalProject/Show.c
@@ -1,47 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 
 #define DX 2 
 #define DY 2
 
-**char getlines_from_file(char *filename)
+void free_lines(char **lines, int nlines)
+{
+	int i;
+	for(i = 0; i < nlines; i++)
+	{
+		free(lines[i]);
+	}
+	free(lines);
+}
+
+int getlines_from_file(char ***lines, char *filename)
 {
 	FILE *f;
 	char *line = NULL;
 	size_t len = 0;
 	int nlines = 128;
 	int i;
-	char **lines = malloc(sizeof(char *) * nlines);
+	void *new_lines;
+		
+	*lines = calloc(sizeof(char *), nlines);
+	if(*lines == NULL)
+	{
+		perror("calloc() ");
+		return -1;
+	}
 
 	f = fopen(filename, "r");
 	if(f == NULL)
 	{
+		free(*lines);
 		perror("fopen() ");
-		return NULL;
+		return -1;
 	}
 
 	i = 0;
 	while((getline(&line, &len, f)) != -1)
 	{
-		lines[i] = line;
+		(*lines)[i] = line;
 		i++;
 		if(i >= nlines - 1)
 		{
-			nlines = lines * 2;
-			new_lines = realloc(lines, sizeof(char *) * nlines);
+			nlines = nlines * 2;
+			new_lines = reallocarray(*lines, sizeof(char *), nlines);
 			if(new_lines == NULL)
 			{
-				//TODO free
-				perror("realloc() ");
-				return NULL;
+				free_lines(*lines, nlines);
+				fclose(f);
+				perror("reallocarray() ");
+				return -1;
 			}
-			lines = new_lines;
+			*lines = new_lines;
 		}
+
+		line = NULL;
 	}
+
+	fclose(f);
+
+	return i;
 }
 
 int main(int argc, char **argv) 
 {
 	WINDOW *frame, *win;
+	char **lines;
+	int nlines = 0;
+
+	if(argc < 2)
+	{
+		fprintf(stderr, "Missing filename\n");
+		return 1;
+	}
+
+	if((nlines = getlines_from_file(&lines, argv[1])) == -1)
+	{
+		return 1;
+	}
+
+	printf("%d\n", nlines);
+	for(int i = 0; i < nlines; i++)
+	{
+		printf("%s", lines[i]);
+	}
 
 	initscr();
 	noecho();
