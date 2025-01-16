ab7301bae224116e92ef5cdaa73ef5a73a5b35c3
Vadim Kotov <vadia55@list.ru>
2025-01-16 19:02:41 +0300

    file output + key arrow movements

--- 01_TerminalProject/Show.c
+++ 01_TerminalProject/Show.c
@@ -1,110 +1,173 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
+#include <limits.h>
 
 #define DX 2 
 #define DY 2
+#define NLINES (LINES - 2 * DY - 2)
+#define NCOLS (COLS - 2 * DX - 2)
 
 void free_lines(char **lines, int nlines)
 {
-	int i;
-	for(i = 0; i < nlines; i++)
-	{
-		free(lines[i]);
-	}
-	free(lines);
+    int i;
+    for(i = 0; i < nlines; i++)
+    {
+        free(lines[i]);
+    }
+    free(lines);
 }
 
 int getlines_from_file(char ***lines, char *filename)
 {
-	FILE *f;
-	char *line = NULL;
-	size_t len = 0;
-	int nlines = 128;
-	int i;
-	void *new_lines;
-		
-	*lines = calloc(sizeof(char *), nlines);
-	if(*lines == NULL)
-	{
-		perror("calloc() ");
-		return -1;
-	}
+    FILE *f;
+    char *line = NULL;
+    size_t len = 0;
+    int nlines = 128;
+    int i;
+    void *new_lines;
 
-	f = fopen(filename, "r");
-	if(f == NULL)
-	{
-		free(*lines);
-		perror("fopen() ");
-		return -1;
-	}
+    *lines = calloc(sizeof(char *), nlines);
+    if(*lines == NULL)
+    {
+        perror("calloc() ");
+        return -1;
+    }
 
-	i = 0;
-	while((getline(&line, &len, f)) != -1)
-	{
-		(*lines)[i] = line;
-		i++;
-		if(i >= nlines - 1)
-		{
-			nlines = nlines * 2;
-			new_lines = reallocarray(*lines, sizeof(char *), nlines);
-			if(new_lines == NULL)
-			{
-				free_lines(*lines, nlines);
-				fclose(f);
-				perror("reallocarray() ");
-				return -1;
-			}
-			*lines = new_lines;
-		}
+    f = fopen(filename, "r");
+    if(f == NULL)
+    {
+        free(*lines);
+        perror("fopen() ");
+        return -1;
+    }
 
-		line = NULL;
-	}
+    i = 0;
+    while((getline(&line, &len, f)) != -1)
+    {
+        (*lines)[i] = line;
+        i++;
+        if(i >= nlines - 1)
+        {
+            nlines = nlines * 2;
+            new_lines = reallocarray(*lines, sizeof(char *), nlines);
+            if(new_lines == NULL)
+            {
+                free_lines(*lines, nlines);
+                fclose(f);
+                perror("reallocarray() ");
+                return -1;
+            }
+            *lines = new_lines;
+        }
+        
+        line = NULL;
+    }
+    free(line);
 
-	fclose(f);
+    fclose(f);
 
-	return i;
+    return i;
 }
 
 int main(int argc, char **argv) 
 {
-	WINDOW *frame, *win;
-	char **lines;
-	int nlines = 0;
+    WINDOW *frame, *win;
+    char **lines;
+    int nlines = 0;
+    int fline = 0, fcol = 0;
+    int c = 0;
+    int refresh;
 
-	if(argc < 2)
-	{
-		fprintf(stderr, "Missing filename\n");
-		return 1;
-	}
+    if(argc < 2)
+    {
+        fprintf(stderr, "Missing filename\n");
+        return 1;
+    }
 
-	if((nlines = getlines_from_file(&lines, argv[1])) == -1)
-	{
-		return 1;
-	}
+    if((nlines = getlines_from_file(&lines, argv[1])) == -1)
+    {
+        return 1;
+    }
 
-	printf("%d\n", nlines);
-	for(int i = 0; i < nlines; i++)
-	{
-		printf("%s", lines[i]);
-	}
+    initscr();
+    noecho();
+    cbreak();
+    curs_set(0);
+    
+    printw("File: %s", argv[1]);
+    refresh();
 
-	initscr();
-	noecho();
-	cbreak();
-	
-	printw("File: %s", argv[1]);
-	refresh();
+    frame = newwin(LINES - 2 * DY, COLS - 2 * DX, DY, DX);
+    box(frame, 0, 0);
+    wrefresh(frame);
 
-	frame = newwin(LINES - 2 * DY, COLS - 2 * DX, DY, DX);
-	box(frame, 0, 0);
-	wrefresh(frame);
+    win = newwin(NLINES, NCOLS, DY + 1, DX + 1);
+    keypad(win, TRUE);
+    wmove(win, 0, 0);
+    while(c != 27)
+    {
+        werase(win);
+        wmove(win, 0, 0);
+        for(int line = fline; line < fline + NLINES && line < nlines; line++)
+        {
+            if(fcol < (int)strlen(lines[line]))
+            {
+                wprintw(win, "%4d:%.*s", line, NCOLS - 5, lines[line] + fcol);
+            }
+            else
+            {
+                wprintw(win, "%4d:\n", line);
+            }
+        }
 
-	win = newwin(LINES - 2 * DY - 2, COLS - 2 * DX - 2, DY + 1, DX + 1);
-	//wmove(win, 0, 1);
-	//wrefresh(win);
-	wgetch(win);
-	endwin();
+        refresh = 0;
+        while(!refresh)
+        {
+            c = wgetch(win);
+            if(c == 27)
+                break;
 
-	return 0;
+            refresh = 1;    
+            switch(c)
+            {
+                case KEY_UP:
+                    if(fline > 0)
+                        fline--;
+                    break;
+
+                case KEY_DOWN:
+                    if(fline + NLINES < nlines)
+                        fline++;
+                    break;
+
+                case KEY_LEFT:
+                    if(fcol > 0)
+                        fcol--;
+                    break;
+
+                case KEY_RIGHT:
+                    if(fcol < INT_MAX)
+                        fcol++;
+                    break;
+
+                default:
+                    refresh = 0;
+            }
+        }
+
+        if(c != 27)
+        {
+            box(frame, 0, 0);
+            wmove(frame, 0, 1);
+            wprintw(frame, "%s", keyname(c));
+            wrefresh(frame);
+        }
+    }
+    endwin();
+
+    free_lines(lines, nlines);
+
+    return 0;
 }
