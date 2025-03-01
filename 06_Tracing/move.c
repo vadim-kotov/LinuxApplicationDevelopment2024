762864b3ae6b89179749fc86d1325babb516a19d
Vadim Kotov <vadia55@list.ru>
2025-03-01 18:17:32 +0300

    move.c changes

--- 06_Tracing/move.c
+++ 06_Tracing/move.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
+
+#define SUCCESS 0
+#define NOARG_ERR 1
+#define FOPEN_ERR 2
+#define READFILE_ERR 3
+#define WRITEFILE_ERR 4
+#define FCLOSE_ERR 5
+#define REMOVEFILE_ERR 6
 
 void usage(char *prog)
 {
-	fprintf(stderr, "Make file copy\n"\
-			"Usage: %s <source file> <destination file>\n", prog);
+	fprintf(stderr, "%s: Make file copy\n"\
+			"Usage: %s <source file> <destination file>\n", prog, prog);
 }
 
 int main(int argc, char **argv)
 {
 	FILE *src, *dest;
 	char ch;
+	char *infile = argv[1], *outfile = argv[2];
 
 	if(argc < 3)
 	{
 		fprintf(stderr, "Missing argument\n");
 		usage(argv[0]);
-		return 1;
+		return NOARG_ERR;
 	}
 
 	src = fopen(argv[1], "r");
 	if(src == NULL)
 	{
 		perror("Source file opening error ");
-		return 1;
+		return FOPEN_ERR;
 	}
 
 	dest = fopen(argv[2], "w");
@@ -31,16 +41,53 @@ int main(int argc, char **argv)
 	{
 		perror("Destination file opening error");
 		fclose(src);
-		return 1;
+		return FOPEN_ERR;
 	}
 
 	while((ch = fgetc(src)) != EOF)
 	{
-		fputc(ch, dest);
+		if((fputc(ch, dest)) == EOF)
+		{
+			ferror(dest);
+			perror("Writing to file error ");
+			fclose(src);
+			fclose(dest);
+	
+			unlink(outfile);
+
+			return WRITEFILE_ERR;
+		}
+	}
+	if(ferror(src))
+	{
+		perror("Reading file error ");
+		fclose(src);
+		fclose(dest);
+
+		unlink(outfile);
+
+		return READFILE_ERR;
 	}
 
-	fclose(src);
-	fclose(dest);
+	if(fclose(src))
+	{
+		perror("Source file close error ");
+		fclose(dest);
+		unlink(outfile);
 
-	return 0;
+		return FCLOSE_ERR;
+	}
+	if(fclose(dest))
+	{
+		perror("Destination file close error ");
+		return FCLOSE_ERR;
+	}
+
+	if(unlink(infile))
+	{
+		perror("File deletion error");
+		return REMOVEFILE_ERR;
+	}
+
+	return SUCCESS;
 }
