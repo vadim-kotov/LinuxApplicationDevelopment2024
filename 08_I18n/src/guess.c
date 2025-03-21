98aebd0687de8fe8db9a6a1cb78ea46e6911a045
Vadim Kotov <vadia55@list.ru>
2025-03-21 08:56:11 +0300

    autotools

--- 08_I18n/src/guess.c
+++ 08_I18n/src/guess.c
@@ -1,36 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+#include <libintl.h>
+#include <locale.h>
+#include "config.h"
+
+#define _(STRING) gettext(STRING)
 
 int main() 
 {
-    int low = 1;
-    int high = 100;
-    int guess;
-    char response;
+	int low = 1;
+	int high = 99;
+	int guess;
+	char resp[2], in;
 
-    printf("Think of a number between 1 and 100\n");
+	setlocale(LC_ALL, "");
+	bindtextdomain(PACKAGE, LOCALEDIR);
+	textdomain(PACKAGE);
 
-    while (low <= high) 
-    {
-        guess = (low + high) / 2;
+	printf(_("Think of a number between 1 and 100\n"));
 
-        printf("Is your number bigger than %d? (y/n): ", guess);
-        scanf(" %c", &response);
-
-        if (response == 'y') 
+	while (low <= high) 
 	{
-            low = guess + 1;
-        } 
-	else if (response == 'n') 
-	{
-            high = guess - 1;
-        } 
-	else 
-	{
-            printf("Please, type 'y'(yes) or 'n'(no).\n");
-        }
-    }
+		guess = (low + high) / 2;
 
-    printf("Your number is: %d!\n", low);
+		printf(_("Is your number bigger than %d? (y/n): "), guess);
+		scanf(" %c", &in);
+		resp[0] = in;
+		resp[1] = '\0';
 
-    return 0;
+		if (!strcmp(resp, _("y")))
+		{
+			low = guess + 1;
+		} 
+		else if (!strcmp(resp, _("n")))
+		{
+			high = guess - 1;
+		} 
+		else
+		{
+			printf(_("Please, type 'y'(yes) or 'n'(no).\n"));
+		}
+	}
+
+	printf(_("Your number is: %d!\n"), low);
+	
+	return 0;
 }
